c893117a1410d0c08086e1466047ad58a515f2f1
Larkin Crain <larkincrain@gmail.com>
2015-02-19 21:46:31 -0600

    Added LED

--- Output/LED/LED.h
+++ Output/LED/LED.h
@@ -1,24 +1,24 @@
+/*
 Title: LED.h
-+* Authors: Clark Asher
-+* Group: The A Team
-+* For: Senior Design - Metering Project
-+* Date: 2/18/2015
-+*
-+* Library definition for controlling LEDs
-+*
-+*/
-+
-+
-+#ifndef LED_h
-+#define LED_h
-+
-+class Bargraph
-+{
-+	public:
-+
-+		Bargraph();
-+	
-+		unsigned int ConvertPower();   //converts instantaneous power to binary data
-+		bool LEDwrite();   //triggers LEDs for bargraph
-+
-+};
+Authors: Clark Asher
+* Group: The A Team
+* For: Senior Design - Metering Project
+* Date: 2/18/2015
+*
+* Library definition for controlling LEDs
+*
+*/
+
+#ifndef LED_h
+#define LED_h
+
+class Bargraph
+{
+	public:
+
+		Bargraph();
+	
+		unsigned int ConvertPower();   //converts instantaneous power to binary data
+		bool LEDwrite();   //triggers LEDs for bargraph
+
+};
